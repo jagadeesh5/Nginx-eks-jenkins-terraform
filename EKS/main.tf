b76f133cff8309e68e4655fedc00a41b23c62944
jagadesh <jagadeeshpalle586@gmail.com>
2024-08-19 09:44:41 +0530

    changes in configuration file

--- EKS/main.tf
+++ EKS/main.tf
@@ -34,11 +34,11 @@ module "vpc" {
 module "eks" {
   source                         = "terraform-aws-modules/eks/aws"
   cluster_name                   = "my-eks-cluster"
-  cluster_version                = "1.30"
+  cluster_version                = "1.29"
   cluster_endpoint_public_access = true
   vpc_id                         = module.vpc.vpc_id
   subnet_ids                     = module.vpc.private_subnets
-  
+
   eks_managed_node_groups = {
     nodes = {
       min_size       = 1
@@ -51,14 +51,12 @@ module "eks" {
     Environment = "dev"
     Terraform   = "true"
   }
-
 }
 
-
 data "aws_eks_cluster" "cluster" {
-  name = module.eks.cluster_name
+  name = module.eks.cluster_id
 }
 
 data "aws_eks_cluster_auth" "cluster" {
-  name = module.eks.cluster_name
+  name = module.eks.cluster_id
 }
